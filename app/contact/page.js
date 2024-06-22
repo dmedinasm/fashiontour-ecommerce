430ce58e8821c237679679533d13259d879ccaad
dmedinas <dmedinas@gmail.com>
2024-06-21 22:47:21 -0500

    Corregidos detalles del diseño responsive

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,102 +2,71 @@ import React from 'react'
 
 function Contact () {
   return (
-    <section className="py-10 bg-gray-100 sm:py-16 lg:py-24 ">
-    <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="max-w-2xl mx-auto text-center">
-            <h2 className="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl">Contact us</h2>
-            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-500">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
-        </div>
+    <section >
+    <div className="relative flex items-top justify-center min-h-screen bg-white  sm:items-center sm:pt-0">
+      <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div className="mt-8 overflow-hidden">
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="p-6 mr-2 bg-gray-100  sm:rounded-lg">
+              <h1 className="text-4xl sm:text-5xl text-gray-800  font-extrabold tracking-tight">
+                Get in touch
+              </h1>
+              <p className="text-normal text-lg sm:text-2xl font-medium text-gray-600 mt-2">
+                Fill in the form to start a conversation
+              </p>
 
-        <div className="max-w-5xl mx-auto mt-12 sm:mt-16">
-            <div className="grid grid-cols-1 gap-6 px-8 text-center md:px-0 md:grid-cols-3">
-                <div className="overflow-hidden bg-white rounded-xl">
-                    <div className="p-6">
-                        <svg className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="1"
-                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                            />
-                        </svg>
-                        <p className="mt-6 text-lg font-medium text-gray-900">+1-316-555-0116</p>
-                        <p className="mt-1 text-lg font-medium text-gray-900">+1-446-526-0117</p>
-                    </div>
+              <div className="flex items-center mt-8 text-gray-600 ">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                </svg>
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  Acme Inc, Street, State, Postal Code
                 </div>
+              </div>
 
-                <div className="overflow-hidden bg-white rounded-xl">
-                    <div className="p-6">
-                        <svg className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                        </svg>
-                        <p className="mt-6 text-lg font-medium text-gray-900">contact@example.com</p>
-                        <p className="mt-1 text-lg font-medium text-gray-900">hr@example.com</p>
-                    </div>
+              <div className="flex items-center mt-4 text-gray-600 ">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
+                </svg>
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  +44 1234567890
                 </div>
+              </div>
 
-                <div className="overflow-hidden bg-white rounded-xl">
-                    <div className="p-6">
-                        <svg className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                        <p className="mt-6 text-lg font-medium leading-relaxed text-gray-900">8502 Preston Rd. Ingle, Maine 98380, USA</p>
-                    </div>
+              <div className="flex items-center mt-2 text-gray-600 ">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+                </svg>
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  info@fashiontour.org
                 </div>
+              </div>
             </div>
 
-            <div className="mt-6 overflow-hidden bg-white rounded-xl">
-                <div className="px-6 py-12 sm:p-12">
-                    <h3 className="text-3xl font-semibold text-center text-gray-900">Send us a message</h3>
+            <form className="p-6 flex flex-col justify-center">
+              <div className="flex flex-col">
+                <label htmlFor="name" className="hidden">Full Name</label>
+                <input type="name" name="name" id="name" placeholder="Full Name" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white  border border-gray-400  text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"/>
+              </div>
 
-                    <form action="#" method="POST" className="mt-14">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
-                            <div>
-                                <label htmlFor="" className="text-base font-medium text-gray-900"> Your name </label>
-                                <div className="mt-2.5 relative">
-                                    <input type="text" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
-                                </div>
-                            </div>
+              <div className="flex flex-col mt-2">
+                <label htmlFor="email" className="hidden">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white  border border-gray-400  text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"/>
+              </div>
 
-                            <div>
-                                <label htmlFor="" className="text-base font-medium text-gray-900"> Email address </label>
-                                <div className="mt-2.5 relative">
-                                    <input type="email" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
-                                </div>
-                            </div>
+              <div className="flex flex-col mt-2">
+                <label htmlFor="tel" className="hidden">Number</label>
+                <input type="tel" name="tel" id="tel" placeholder="Telephone Number" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white  border border-gray-400  text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"/>
+              </div>
 
-                            <div>
-                                <label htmlFor="" className="text-base font-medium text-gray-900"> Phone number </label>
-                                <div className="mt-2.5 relative">
-                                    <input type="tel" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
-                                </div>
-                            </div>
-
-                            <div>
-                                <label htmlFor="" className="text-base font-medium text-gray-900"> Company name </label>
-                                <div className="mt-2.5 relative">
-                                    <input type="text" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
-                                </div>
-                            </div>
-
-                            <div className="sm:col-span-2">
-                                <label htmlFor="" className="text-base font-medium text-gray-900"> Message </label>
-                                <div className="mt-2.5 relative">
-                                    <textarea name="" id="" placeholder="" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600" rows="4"></textarea>
-                                </div>
-                            </div>
-
-                            <div className="sm:col-span-2">
-                                <button type="submit" className="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-primary border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700">
-                                    Send
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+              <button type="submit" className="md:w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg mt-3  transition ease-in-out duration-300">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
 </section>
 
