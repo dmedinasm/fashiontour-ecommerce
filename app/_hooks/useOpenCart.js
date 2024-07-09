96d9bbc1cfa07ae0a5e8729f5e2e961b83ae53cd
dmedinas <dmedinas@gmail.com>
2024-07-09 16:57:35 -0500

    Arreglado detalle en logica para mostrar carrito

--- app/_hooks/useOpenCart.js
+++ app/_hooks/useOpenCart.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import { useCartStore } from '../_store/cartStore'
+import { useUser } from '@clerk/nextjs'
 export function useOpenCart () {
+  const { isSignedIn } = useUser()
   const [openCart, setOpenCart] = useState(false)
   const cart = useCartStore(state => state.cart)
   useEffect(() => {
-    setOpenCart(true)
-  }, [cart])
+    isSignedIn && setOpenCart(true)
+  }, [cart, isSignedIn])
   return { openCart, setOpenCart }
 }
