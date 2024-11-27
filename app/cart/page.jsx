1612c20b403f1ecda8cfd08dc527d3a4279e8de1
dmedinas <dmedinas@gmail.com>
2024-11-27 08:37:20 -0500

    Optimizado redireccionamiento en caso de usuario no autenticado en ruta cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,23 +1,28 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CartDetail from '../cart/CartDetail'
 import { auth } from '../lib/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useRouter } from 'next/navigation'
 import { useCartStore } from '../_store/cartStore'
+
 export default function CartItems () {
-  const route = useRouter()
-  const [user] = useAuthState(auth)
+  const router = useRouter()
+  const [user, loading] = useAuthState(auth)
   const { isTryLogin } = useCartStore()
 
-  if (!user) {
-    route.push('/sign-in')
-    isTryLogin(true)
-    return
+  useEffect(() => {
+    if (!loading) {
+      if (!user) {
+        router.push('/sign-in')
+        isTryLogin(true)
+      }
+    }
+  }, [user, loading, router, isTryLogin])
+
+  if (loading || !user) {
+    return <div className='h-screen'></div>
   }
-  return (
-   <>
-    <CartDetail/>
-   </>
-  )
+
+  return <CartDetail />
 }
