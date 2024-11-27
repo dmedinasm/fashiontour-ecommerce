05d1775b14240bb3177b0dd1cd14f4c864bd705b
dmedinas <dmedinas@gmail.com>
2024-11-27 09:58:23 -0500

    Optimizado carga de la ruta checkout

---
+++ app/checkout/_components/CheckoutContent.jsx
@@ -0,0 +1,44 @@
+'use client'
+import React, { useEffect } from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import CheckoutForm from '../_components/CheckoutForm'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../../lib/firebase'
+import { useCartStore } from '../../_store/cartStore'
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+const CheckoutContent = () => {
+  const searchParams = useSearchParams()
+  const options = {
+    mode: 'payment',
+    currency: 'usd',
+    amount: searchParams.get('amount') * 100
+  }
+  const route = useRouter()
+  const { isTryLogin } = useCartStore()
+  const [user, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    if (!loading) {
+      if (!user) {
+        route.push('/sign-in')
+        isTryLogin(true)
+      }
+    }
+  }, [user, loading, route, isTryLogin])
+
+  if (loading || !user) {
+    return <div className='h-screen'></div>
+  }
+
+  return (
+    <div className='mx-auto my-40 ss:max-w-5xl'>
+      <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm amount={Number(searchParams.get('amount'))} />
+    </Elements>
+    </div>
+  )
+}
+
+export default CheckoutContent
--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,37 +1,10 @@
-'use client'
-import React from 'react'
-import { loadStripe } from '@stripe/stripe-js'
-import { Elements } from '@stripe/react-stripe-js'
-import CheckoutForm from './_components/CheckoutForm'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '../lib/firebase'
-import { useCartStore } from '../_store/cartStore'
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
-const Checkout = () => {
-  const searchParams = useSearchParams()
-  const options = {
-    mode: 'payment',
-    currency: 'usd',
-    amount: searchParams.get('amount') * 100
-  }
-  const route = useRouter()
-  const { isTryLogin } = useCartStore()
-  const [user] = useAuthState(auth)
-
-  if (!user) {
-    route.push('/sign-in')
-    isTryLogin(true)
-    return
-  }
-
+import React, { Suspense } from 'react'
+import CheckoutContent from './_components/CheckoutContent'
+import SkeletonCheck from './_components/SkeletonCheck'
+export default function page () {
   return (
-    <div className='mx-auto my-40 ss:max-w-5xl'>
-      <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm amount={Number(searchParams.get('amount'))} />
-    </Elements>
-    </div>
+    <Suspense fallback={<SkeletonCheck/>}>
+      <CheckoutContent/>
+    </Suspense>
   )
 }
-
-export default Checkout
